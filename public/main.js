0db0edc786bf09fc758e5864eaf39474cd34d25f
Daniel Duma <dduma@Daniels-Air.home>
2015-05-24 11:45:41 -0400

    user join/leave messages, nicknames, total users

--- public/main.js
+++ public/main.js
@@ -4,13 +4,24 @@ $(document).ready(function() {
 	var socket = io();
 
 	//use jQuery to select the <input> tag
-	var input = $('input');
+	var input = $('#message-box');
+	var nicknameInput = $('#nickname-box');
 	//use jQuery to select the <div> tag with id=messages
 	//does hash (#) denote id?
 	var messages = $('#messages');
+	var users = $('#users');
+	var nickname;
 
 	var addMessage = function(message) {
 		messages.append('<div>' + message + '</div>');
+	};
+
+	var joinLeave = function(message) {
+		messages.append('<div><i>' + message + '</i></div>');
+	};
+
+	var updateUserCount = function(userCount) {
+		users.text(userCount);
 	};
 
 	//adds a 'listener' named keydown to the input tag
@@ -21,9 +32,20 @@ $(document).ready(function() {
 
 		var message = input.val();
 		addMessage(message); //add <div>message</div> to #message div\
-		socket.emit('message', message);
+		socket.emit('message', nickname + ': ' + message);
 		input.val(''); //clear input field
 	});
 
+	nicknameInput.on('keydown', function(event) {
+		if (event.keyCode != 13) {
+			return;
+		}
+
+		nickname = nicknameInput.val();
+		socket.emit('set-nickname', nickname);
+	});
+
 	socket.on('message', addMessage);
+	socket.on('joinleave', joinLeave);
+	socket.on('users', updateUserCount);
 })
--- server.js
+++ server.js
@@ -6,17 +6,33 @@ var app = express();
 app.use(express.static('public'));
 
 var server = http.Server(app);
-var io = socket_io(server); 
 // io object is a socket.io server
 // and an instance of EventEmitter
+var io = socket_io(server); 
+var totalClients = 0;
 
 io.on('connection', function(socket) {
 	console.log('Client connected');
+	io.emit('users', ++totalClients);
+	socket.broadcast.emit('joinleave', 'someone joined');
+	socket.nickname = '';
 
 	socket.on('message', function(message) {
 		console.log('Received message:', message);
 		//send message to all clients except socket in use
 		socket.broadcast.emit('message', message);
+	});
+
+	socket.on('set-nickname', function(nickname) {
+		console.log(nickname + ' nickname set');
+		this.nickname = nickname;
+	});
+
+	socket.on('disconnect', function() {
+		var who = this.nickname == '' ? 'someone' : this.nickname; 
+		console.log(who + ' disconnected');
+		io.emit('users', --totalClients);
+		io.emit('joinleave', who + ' left...');
 	});
 });
 
